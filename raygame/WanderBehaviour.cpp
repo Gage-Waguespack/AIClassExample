c64a1015c6481cb12c829d31028bab4940a8c3db
Gage <gagewaguespack@gmail.com>
2021-03-03 16:12:33 -0600

    Started the framework for wander and added comments to guide me

--- raygame/WanderBehaviour.cpp
+++ raygame/WanderBehaviour.cpp
@@ -1,1 +1,31 @@
 #include "WanderBehaviour.h"
+#include <vector>
+
+WanderBehaviour::WanderBehaviour()
+{
+	//Calculate the circle center
+
+	//Calculate the displacement force
+
+
+	//Randomly change the vector direction 
+	//by making it change its current angle
+
+
+	//Change wanderAngle just a bit, so it won't
+	//have the same value in the next game frame
+
+
+	//Finally calculate and return the wander force
+
+}
+
+void WanderBehaviour::setAngle()
+{
+	
+}
+
+void WanderBehaviour::update(Agent* agent, float deltatime)
+{
+
+}
--- raygame/WanderBehaviour.h
+++ raygame/WanderBehaviour.h
@@ -1,7 +1,13 @@
 #pragma once
 #include "Behaviour.h"
+#include <Vector2.h>
 class WanderBehaviour : public Behaviour
 {
-
+public:
+	WanderBehaviour();
+	void setAngle();
+	void update(Agent* agent, float deltatime) override;
+private:
+	Agent* m_target;
+	float m_wanderForce;
 };
-
