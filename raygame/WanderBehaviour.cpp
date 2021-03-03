60901c76979b097e612ad9388e9fd348d6a715ba
Gage <gagewaguespack@gmail.com>
2021-03-03 16:34:59 -0600

    Added my own personalized instructions to help with implementation

--- raygame/WanderBehaviour.cpp
+++ raygame/WanderBehaviour.cpp
@@ -3,6 +3,14 @@
 
 WanderBehaviour::WanderBehaviour()
 {
+	// I need to make a circle, then I need to offset the circle 
+	//and calculate the center. After that I have to pick a random 
+	//point normalized to the circles border and calculate the 
+	//displacement and with that displacement, return the wander
+	//force, and move to that random point and keep doing that 
+	//until the enemy stops wandering (whenevever that may be).
+
+
 	//Calculate the circle center
 
 
